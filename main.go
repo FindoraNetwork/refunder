0727ed8e61c4fb0cbcc283451a4845b2b3af7229
tommady <tommady@users.noreply.github.com>
2022-02-11 21:21:05 +0800

    using basic config from ENV

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://prod-testnet.prod.findora.org:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = client
 }
